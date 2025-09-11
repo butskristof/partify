df5ea35ced93715df4db42f83739e348fca58a42
Kristof Buts <git@kristofbuts.be>
2025-09-11 20:14:35 +0200

    primevue setup

--- frontend/app/components/app/AppShell.vue
+++ frontend/app/components/app/AppShell.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>Partify</h1>
+  <div class="app-shell">
+    <h1>Partify</h1>
+    <PrimeTest />
+  </div>
 </template>
+
+<script setup lang="ts">
+import PrimeTest from '~/components/app/PrimeTest.vue';
+</script>
+
+<style scoped lang="scss">
+.app-shell {
+  background-color: var(--p-surface-500);
+  color: var(--color-text);
+  padding: var(--default-spacing);
+}
+</style>
---
+++ frontend/app/components/app/PrimeTest.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="prime-test">
+    <h2>prime test</h2>
+
+    <div>
+      <InputText placeholder="Enter a value here" />
+    </div>
+
+    <div>
+      <InputText
+        disabled
+        placeholder="Disabled"
+      />
+    </div>
+
+    <div class="buttons">
+      <Button label="Submit" />
+      <Button
+        label="Profile"
+        icon="pi pi-user"
+      />
+      <Button
+        type="button"
+        label="Search"
+        icon="pi pi-search"
+        :loading="true"
+      />
+      <ToggleSwitch />
+    </div>
+
+    <div class="progress-bar">
+      <ProgressBar :value="50" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.prime-test {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+.buttons {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+</style>
