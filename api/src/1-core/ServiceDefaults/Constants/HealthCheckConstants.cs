a22c5b9f3b8469ebec29a4c4e8a54ca6b1674180
Kristof Buts <git@kristofbuts.be>
2025-09-12 11:57:44 +0200

    update ServiceDefaults

---
+++ api/src/1-core/ServiceDefaults/Constants/HealthCheckConstants.cs
@@ -0,0 +1,20 @@
+namespace Partify.ServiceDefaults.Constants;
+
+public static class HealthCheckConstants
+{
+    internal static class Endpoints
+    {
+        internal const string BasePath = "/health";
+    }
+
+    public static class Tags
+    {
+        public const string Live = "live";
+        public const string Ready = "ready";
+    }
+
+    internal static class Names
+    {
+        internal const string Self = "self";
+    }
+}
