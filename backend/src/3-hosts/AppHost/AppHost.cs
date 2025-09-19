0829b92f75741865ad4ba20397a6021c02879ab5
Kristof Buts <git@kristofbuts.be>
2025-09-19 14:21:43 +0200

    aspire setup

--- backend/src/3-hosts/AppHost/AppHost.cs
+++ backend/src/3-hosts/AppHost/AppHost.cs
@@ -1,3 +1,32 @@
+using Partify.ServiceDefaults.Constants;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+#region Database
+
+var postgres = builder
+    .AddPostgres(Resources.Postgres)
+    .WithPgAdmin()
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithDataVolume();
+var appDb = postgres.AddDatabase(Resources.AppDb);
+
+var databaseMigrations = builder
+    .AddProject<Projects.DatabaseMigrations>(Resources.DatabaseMigrations)
+    .WithReference(appDb)
+    .WaitFor(appDb);
+
+#endregion
+
+#region Backend
+
+var backend = builder
+    .AddProject<Projects.Web>(Resources.Backend)
+    .WithReference(appDb)
+    .WaitForCompletion(databaseMigrations)
+    .WithHttpHealthCheck("/health/live")
+    .WithHttpHealthCheck("/health/ready");
+
+#endregion
 
 builder.Build().Run();
