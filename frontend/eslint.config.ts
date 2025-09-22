a63b72a8b3f291a7c92d2e897b569cfa772ec0f6
Kristof Buts <git@kristofbuts.be>
2025-09-22 13:08:53 +0200

    eslint scoped css

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -2,32 +2,37 @@ import { globalIgnores } from 'eslint/config';
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
 import pluginVue from 'eslint-plugin-vue';
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
+import pluginVueScopedCss from 'eslint-plugin-vue-scoped-css';
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
 // configureVueProject({ scriptLangs: ['ts', 'tsx'] })
 // More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
 
-export default defineConfigWithVueTs(
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{js,jsx,ts,mts,tsx,vue}'],
-  },
+export default [
+  ...defineConfigWithVueTs(
+    {
+      name: 'app/files-to-lint',
+      files: ['**/*.{js,jsx,ts,mts,tsx,vue}'],
+    },
 
-  globalIgnores([
-    '**/node_modules/**',
-    '**/dist/**',
-    '**/dist-ssr/**',
-    '**/coverage/**',
-    '**/.vscode/**',
-    '**/.idea/**',
-    '**/*.min.js',
-    '**/public/**',
-    '**/build/**',
-  ]),
+    globalIgnores([
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/dist-ssr/**',
+      '**/coverage/**',
+      '**/.vscode/**',
+      '**/.idea/**',
+      '**/*.min.js',
+      '**/public/**',
+      '**/build/**',
+    ]),
 
-  pluginVue.configs['flat/recommended'],
-  vueTsConfigs.recommended,
+    pluginVue.configs['flat/recommended'],
+    vueTsConfigs.recommended,
+  ),
+
+  ...pluginVueScopedCss.configs['flat/recommended'],
   skipFormatting,
   {
     name: 'app/custom-rules',
@@ -42,4 +47,4 @@ export default defineConfigWithVueTs(
       ],
     },
   },
-);
+];
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,12 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
+// @ts-expect-error https://github.com/gxmari007/vite-plugin-eslint/issues/79
+import eslint from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), vueDevTools(), eslint()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
